06f0571de77bcbf9fe02ff65e5c40cda153b7b2e
juandev407 <jjmka407@gmail.com>
2021-10-10 22:44:28 +0000

    Delete serializers and views to add another commit

--- supplierApp/serializers/supplierSerializer.py
+++
@@ -1,6 +0,0 @@
-from supplierApp.models.supplier import Supplier
-from rest_framework import serializers
-class SupplierSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Supplier
-        fields = ['name', 'phone', 'email', 'address','city']
