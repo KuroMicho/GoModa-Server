842a5ac8f2b37f8f5238df32bde74b95b8eda059
juandev407 <jjmka407@gmail.com>
2021-10-11 00:01:34 +0000

    Create Django app with product Model

---
+++ products/models/product.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+def jsonfield_default_value():
+    return {}
+
+
+class Product(models.Model):
+
+    barcode = models.CharField(
+        blank=False, unique=True, default="123456789", max_length=10
+    )
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True)
+    color = models.JSONField(blank=True, default=jsonfield_default_value)
+    material = models.JSONField(blank=True, default=jsonfield_default_value)
+    size = models.JSONField(blank=True, default=jsonfield_default_value)
+    price = models.FloatField(blank=False, default=0)
+    inventory_received = models.IntegerField(blank=True, default=0)
+    minimum_required = models.IntegerField(blank=False)
+    inventory_onhand = models.IntegerField(blank=True, default=0)
+    inventory_shipped = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return self.name
