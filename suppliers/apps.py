e183db3d2dfc2bd22de082fd6e2c9dcb10ded075
KuroMicho <kevin0552009.kr@gmail.com>
2021-10-12 21:50:04 -0500

    adding BigAutoField to apps

--- suppliers/apps.py
+++ suppliers/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SupplierappConfig(AppConfig):
+class SuppliersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'supplierApp'
+    name = 'suppliers'
