6424f003fcf3884cf54d4bcab686de030344bcbd
juandev407 <jjmka407@gmail.com>
2021-10-14 03:59:00 +0000

    Add serializer_class variable to avoid an error

--- products/views/productCreateView.py
+++ products/views/productCreateView.py
@@ -1,11 +1,11 @@
 from rest_framework.response import Response
 from rest_framework import generics, status
-from products.models.product import Product
 from products.serializers.productSerializer import ProductSerializer
 
 
 class ProductCreateView(generics.CreateAPIView):
 
+    serializer_class = ProductSerializer
     def post(self, request, *args, **kwargs):
 
         serializer = ProductSerializer(data=request.data)
