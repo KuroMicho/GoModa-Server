11cc680b93d26aeb1aa639d87ff07d498f93bb44
juandev407 <jjmka407@gmail.com>
2021-10-17 02:52:04 +0000

    Check if the id exists before executing a filter

--- purchases/views/purchasesRetrieveView.py
+++ purchases/views/purchasesRetrieveView.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import generics, status
 from purchases.models.purchase import Purchase
+from products.models.product import Product
 from purchases.serializers.purchaseSerializer import PurchaseSerializer
 
 
@@ -8,7 +9,12 @@ class PurchasesRetrieveView(generics.RetrieveAPIView):
 
     def get(self, request, *args, **kwargs):
 
-        queryset = Purchase.objects.filter(product=kwargs.get('product_id')).all()
+        try:
+            product = Product.objects.get(id=kwargs.get("product_id"))
+        except Exception as e:
+            return Response({"status": "Not Found"}, status=status.HTTP_404_NOT_FOUND)
+
+        queryset = Purchase.objects.filter(product=product.id).all()
         serializer = PurchaseSerializer(queryset, many=True)
         
         return Response(serializer.data, status=status.HTTP_200_OK)
