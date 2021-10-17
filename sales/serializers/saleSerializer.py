5aa9d2357ae976600ab4151835001ef06bf2bf78
juandev407 <jjmka407@gmail.com>
2021-10-17 01:31:22 +0000

    Update to_representation method of the saleSerializer

--- sales/serializers/saleSerializer.py
+++ sales/serializers/saleSerializer.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
 from sales.models.sale import Sale
+from products.models.product import Product
 
 
 class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
         fields = '__all__'
+
+    def to_representation(self, obj):
+        sale    = Sale.objects.get(id=obj.id)
+        product = Product.objects.get(sale=obj.id)
+
+        return {
+            'sale_id'      : sale.id,
+            'date_shipped' : sale.date_shipped,
+            'product' : {
+                'id'               : product.id,
+                'name'             : product.name,
+                'price'            : product.price,
+                'number_shipments' : sale.number_shipments
+            }
+        }
