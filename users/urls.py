c0e72744a17a1f879c4166fac403ee5e99c2845e
KuroMicho <kevin0552009.kr@gmail.com>
2021-10-19 15:03:45 -0500

    login urls and views

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('csrf/', views.get_csrf, name='api-csrf'),
     path('register/', views.Register.as_view({'post': 'create'})),
     path('login/refresh/', TokenRefreshView.as_view()),
+    path('login/', views.Login.as_view()),
+    path('logout/', views.Logout.as_view()),
     path('user/', views.Profile.as_view()),
     path('users/', views.Users.as_view()),
     path('users/<int:user_id>', views.UserDetail.as_view()),
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 # django
+from django.contrib.auth import authenticate
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.middleware.csrf import get_token
@@ -8,10 +9,12 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 # permissions
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 # models
 from users.models import User
 # serializers
 from users.serializers import CustomUserSerializer, UserSerializer
+from users.serializers.serializer import CustomStorageUserSerializer, CustomTokenObtainPairSerializer
 
 
 # registration
@@ -24,6 +27,28 @@ class Register(viewsets.GenericViewSet):
             user_serializer.save()
             return Response({"msg": "User registered successfully"}, status=status.HTTP_201_CREATED)
         return Response({"msg": "Error found", "error": user_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+# login
+class Login(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        username = request.data.get('username', '')
+        password = request.data.get('password', '')
+        user = authenticate(username=username, password=password)
+        if user:
+            login_serializer = self.serializer_class(data=request.data)
+            if login_serializer.is_valid():
+                user_serializer = CustomStorageUserSerializer(user)
+                return Response({
+                    'access_token': login_serializer.validated_data.get('access'),
+                    'refresh_token': login_serializer.validated_data.get('refresh'),
+                    'user': user_serializer.data,
+                    'msg': 'Logged in Succesfull'
+                }, status=status.HTTP_200_OK)
+            return Response({'status':'error', 'msg': 'Username or password invalid'},status=status.HTTP_400_BAD_REQUEST)
+        return Response({'status':'error', 'msg': 'User not found'},status=status.HTTP_400_BAD_REQUEST)
+
 
 @ensure_csrf_cookie
 def get_csrf(request):
