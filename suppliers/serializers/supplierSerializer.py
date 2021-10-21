6517914bab4e27b8815c2d39667b98fcfceb159b
juan-skill <1294@holbertonschool.com>
2021-10-21 20:34:15 +0000

    Delete comments

--- suppliers/serializers/supplierSerializer.py
+++ suppliers/serializers/supplierSerializer.py
@@ -4,10 +4,5 @@ from rest_framework import serializers
 class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supplier
-        #fields = '__all__'
-        #fields = ['id','name', 'address', 'phone', 'email', 'city']
         fields = ['name', 'address', 'phone', 'email', 'city']
-
-#Los serializers son clases que nos permiten transformar datos de 
-#formatos propios de Django, en formato del entorno web (JSON en este caso)
      
