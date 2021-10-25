b5d25ecc206e59fbbc37b3d88424467738fd682e
jjmka407 <jjmka407@gmail.com>
2021-10-25 15:14:54 +0000

    Rename user endpoints

--- users/urls.py
+++ users/urls.py
@@ -4,11 +4,11 @@ from users import views
 
 urlpatterns = [
     path('csrf/', views.get_csrf, name='api-csrf'),
-    path('register/', views.Register.as_view({'post': 'create'})),
-    path('login/refresh/', TokenRefreshView.as_view()),
+    path('user/', views.Register.as_view({'post': 'create'})),
+    path('refresh/', TokenRefreshView.as_view()),
     path('login/', views.Login.as_view()),
     path('logout/', views.Logout.as_view()),
-    path('user/', views.Profile.as_view()),
+    path('profile/', views.Profile.as_view()),
     path('users/', views.Users.as_view()),
-    path('users/<int:user_id>', views.UserDetail.as_view()),
+    path('user/<int:id>/', views.UserDetail.as_view()),
 ]
